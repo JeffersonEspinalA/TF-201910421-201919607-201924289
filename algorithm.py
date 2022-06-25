152c1aa2e83b9590d224b605442859f20656e961
Fulgencio Marx Vilca Morales <56830946+BLOOOP788@users.noreply.github.com>
2022-06-25 10:59:22 -0500

    Incluir las funciones de coordenadas y generación de grafo en el archivo algorithm.py #14

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,53 @@
 import json
+import hito_3
 
 
-def graph():
-    Loc = [(10, 10), (10, 24), (23, 22), (22, 11)]
-    G = [[(1, 2), (3, 1)],
-         [(2, 3)],
-         [(3, 3)],
-         []]
-
-    response = {"loc": Loc, "g": G}
+def graph():    
+    calles = []
+    coordenadas = []
+    calles=hito_3.leerCalles()
+    I = hito_3.generarIntersecciones(calles, "source/Intersecciones con coordenadas.txt", 42, 44, coordenadas)
+    hito_3.generarListaAdyacencia(I, coordenadas, 9)
+    #Loc = [(10, 10), (10, 24), (23, 22), (22, 11)]
+    G = []
+    with open("source/Lista_Adyacencia_Generado.txt") as f:
+        for line in f:
+            nums = [float(x) for x in line.split()]
+            G.append([])
+            for i in range(0, len(nums), 2):
+                G[-1].append((int(nums[i]), nums[i+1]))
+    
+    response = {"loc": coordenadas, "g": G}
 
     return json.dumps(response)
 
 def paths():
-    bestpath = [-1, 0, 1, 0]
-    path1 = [-1, 0, 1, 0]
-    path2 = [-1, 0, 1, 0]
+    calles = []
+    coordenadas = []
+    calles=hito_3.leerCalles()
+    I = hito_3.generarIntersecciones(calles, "source/Intersecciones con coordenadas.txt", 42, 44, coordenadas)
+    hito_3.generarListaAdyacencia(I, coordenadas, 9)
+    #Loc = [(10, 10), (10, 24), (23, 22), (22, 11)]
+    G = []
+    with open("source/Lista_Adyacencia_Generado.txt") as f:
+        for line in f:
+            nums = [float(x) for x in line.split()]
+            G.append([])
+            for i in range(0, len(nums), 2):
+                G[-1].append((int(nums[i]), nums[i+1]))
+    op1 = 1
+    op2 = 38
+
+    if (op1 >= 0 and op1 <= 1728) and (op2 >= 0 and op2 <= 1728):
+        shortest_paths = []
+        p = 0
+        for _ in range(3):
+            n_path, p = hito_3.function_path(G, op1, op2, p)
+            shortest_paths.append(n_path)
+    
+        bestpath = shortest_paths[0]
+        path1 = shortest_paths[1]
+        path2 = shortest_paths[2]
 
     response = {"bestpath": bestpath, "path1": path1, "path2": path2}
 
